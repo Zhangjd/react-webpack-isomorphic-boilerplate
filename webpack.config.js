992b81259f9afdbe9630a9723de5a3ecf93e9060
alan <ggddll123@qq.com>
2016-06-28 07:32:37 +0800

    ie8 dev

--- webpack.config.js
+++ webpack.config.js
@@ -99,6 +99,42 @@ if(TARGET === 'dev' || !TARGET) {
   });
 }
 
+
+if(TARGET === 'dev-ie8' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: PATHS.dist,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: false,
+      hot: false,
+      inline: false,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: '0.0.0.0',
+      port: process.env.PORT
+    },
+    plugins: [
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
+  });
+}
+
 if(TARGET === 'build' || TARGET === 'production') {
   module.exports = merge(common, {
     plugins:[
